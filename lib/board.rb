442dc9ba0e5849272643b2dd335b4a121392abf5
Halima-C <67638860+Halima-C@users.noreply.github.com>
2020-07-23 23:20:20 +0200

    Add files via upload

--- lib/board.rb
+++ lib/board.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 
 class Board 
-	attr_accessor :array_boardcase, :count_turn, 
+	attr_accessor :array_boardcase, :count_turn 
 	  #TO DO : la classe a 1 attr_accessor : un array/hash qui contient les BoardCases.
   #Optionnellement on peut aussi lui rajouter un autre sous le nom @count_turn pour compter le nombre de coups joué
 
@@ -19,11 +19,23 @@ class Board
 
     #1) demande au bon joueur ce qu'il souhaite faire
     #2) change la BoardCase jouée en fonction de la valeur du joueur (X ou O)
+    
     puts " Choissisez la case à remplir "
     print ">"
     insert = gets.chomp.to_s
-    @board[insert]= "X"
+    if count_turn % 2 == 0
+    @board[insert] = "X"
+    else @board[insert] = "O"
+    end
 		board
+    @count_turn += 1
+    puts @count_turn
+    
+    if count_turn % 2 == 0
+      puts " Player 1 à toi de jouer !"
+    else puts "Player 2 à toi de jouer !"
+    end
+
 	end
 
   def victory?
@@ -33,8 +45,8 @@ class Board
 			@board["A2"] == "X" && @board["B2"] == "X" && @board["C2"] == "X" || @board["A3"] == "X" && @board["B3"] == "X" && @board["C3"] == "X" || @board["A1"] == "X" && @board["B2"] == "X" && @board["C3"] == "X" || @board["A3"] == "X" && @board["B2"] == "X" && @board["C1"] == "X"    
     	puts "Bravo Joueur 1 ! Tu as gagné !! Par contre Joueur 2, t'es nul."
     	@status = "endgame"   
-    elsif @board["A1"] && @board["A2"] && @board["A3"] == "O" || @board["B1"] && @board["B2"] && @board["B3"] == "0" || @board["C1"] && @board["C2"] && @board["C3"] == "0" || @board["A1"] && @board["B1"] && @board["C1"] == "0" ||
-			@board["A2"] && @board["B2"] && @board["C2"] == "0" || @board["A3"] && @board["B3"] && @board["C3"] == "0" || @board["A1"] && @board["B2"] && @board["C3"] == "0" || @board["A3"] && @board["B2"] && @board["C1"] == "0"           
+    elsif @board["A1"] == "O" && @board["A2"] == "O" && @board["A3"] == "O" || @board["B1"] == "O" && @board["B2"] == "O" && @board["B3"] == "O" || @board["C1"] == "O" && @board["C2"] == "O" && @board["C3"] == "O" || @board["A1"] == "O" && @board["B1"] == "O" && @board["C1"] == "O" ||
+      @board["A2"] == "O" && @board["B2"] == "O" && @board["C2"] == "O" || @board["A3"] == "O" && @board["B3"] == "O" && @board["C3"] == "O" || @board["A1"] == "O" && @board["B2"] == "O" && @board["C3"] == "O" || @board["A3"] == "O" && @board["B2"] == "O" && @board["C1"] == "O" 
     	puts "Bravo Joueur 2 ! Tu as gagné !! Par contre Joueur 1, t'es nul." 
     	@status = "endgame"     
     end
@@ -50,8 +62,8 @@ class Board
   	
   end
 
-
 end
+
 
 
 
