314361c73be0fde800b1e3198af65ebd0c31917e
Halima-C <67638860+Halima-C@users.noreply.github.com>
2020-07-24 08:04:16 +0200

    Update board.rb

--- lib/board.rb
+++ lib/board.rb
@@ -3,22 +3,16 @@ require 'pry'
 
 class Board 
 	attr_accessor :array_boardcase, :count_turn 
-	  #TO DO : la classe a 1 attr_accessor : un array/hash qui contient les BoardCases.
-  #Optionnellement on peut aussi lui rajouter un autre sous le nom @count_turn pour compter le nombre de coups joué
+	  
 
   def initialize()
-    #TO DO :
-    #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
-    #Ces instances sont rangées dans un array/hash qui est l'attr_accessor de la classe
+   
     @count_turn = 0
     @board = {"A1"=>"","B1"=>"", "C1"=>"","A2"=>"","B2"=>"","C2"=>"","A3"=>"","B3"=>"","C3"=>""}
   end
 
   def play_turn
-    #TO DO : une méthode qui :
-
-    #1) demande au bon joueur ce qu'il souhaite faire
-    #2) change la BoardCase jouée en fonction de la valeur du joueur (X ou O)
+    
     
     puts " Choissisez la case à remplir "
     print ">"
@@ -38,7 +32,7 @@ class Board
 	end
 
   def victory?
-    #TO DO : une méthode qui vérifie le plateau et indique s'il y a un vainqueur ou match nul
+   
     # Il y a 8 combinaisons gagnantes
     if @board["A1"] == "X" && @board["A2"] == "X" && @board["A3"] == "X" || @board["B1"] == "X" && @board["B2"] == "X" && @board["B3"] == "X" || @board["C1"] == "X" && @board["C2"] == "X" && @board["C3"] == "X" || @board["A1"] == "X" && @board["B1"] == "X" && @board["C1"] == "X" ||
 			@board["A2"] == "X" && @board["B2"] == "X" && @board["C2"] == "X" || @board["A3"] == "X" && @board["B3"] == "X" && @board["C3"] == "X" || @board["A1"] == "X" && @board["B2"] == "X" && @board["C3"] == "X" || @board["A3"] == "X" && @board["B2"] == "X" && @board["C1"] == "X"    
