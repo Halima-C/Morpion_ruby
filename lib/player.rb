97bb156534b039f16b97b23871f90c56ca7a5b5b
Halima-C <67638860+Halima-C@users.noreply.github.com>
2020-07-24 08:05:08 +0200

    Update player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -4,7 +4,6 @@ class Player
 	attr_reader :name, :player_value
   
   def initialize(name,player_value)
-    #TO DO : doit régler son nom et sa valeur
     @name = name
     @player_value = player_value
   end
